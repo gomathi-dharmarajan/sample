789d77cf7a2b78670e9953618d467ba3da795a74
gomathi.dharmarajan <gomathi.dharmarajan@IN1LT1601>
2022-01-19 08:48:28 +0530

    add

--- sampleapplication/src/sampleapplication/Sample.java
+++ sampleapplication/src/sampleapplication/Sample.java
@@ -1,9 +1,19 @@
 package sampleapplication;
 
+import java.util.Scanner;
+
 public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		
+		System.out.println(c);
+
 		
 		System.out.println("welcome");
 	}
