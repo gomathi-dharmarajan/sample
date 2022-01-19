e6282bf07570dafea50644db3a781a6ea8ed83ad
gomathi.dharmarajan <gomathi.dharmarajan@IN1LT1601>
2022-01-19 16:10:42 +0530

    reverse

--- Sampleproject/src/sample/Samplemessage.java
+++ Sampleproject/src/sample/Samplemessage.java
@@ -1,11 +1,24 @@
 package sample;
 
+import java.util.Scanner;
+
 public class Samplemessage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("kk");
-
+		System.out.println("welcome");
+		int n,r;
+		int s=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		while(n>0)
+		{
+			r=n%10;
+			s=(s*10)+r;
+			n=n/10;
+			
+		}
+		System.out.println(s);
 	}
 
 }
